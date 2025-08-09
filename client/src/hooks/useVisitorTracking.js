dfac7bf3530fdc5c7f6d4d0c21c72dce9fde353e
ranjanarun7 <ranjanarunbabu@gmail.com>
2025-08-10 03:50:39 +0530

    BUG FIX

--- client/src/hooks/useVisitorTracking.js
+++ client/src/hooks/useVisitorTracking.js
@@ -4,17 +4,20 @@ import { useLocation } from "react-router-dom";
 
 export default function useVisitorTracking() {
   const location = useLocation();
+  const BACKEND_URL = import.meta.env.VITE_API_URL;
+
 
   useEffect(() => {
     const token = localStorage.getItem("token");
 
-    fetch("/api/track", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        ...(token ? { Authorization: `Bearer ${token}` } : {}),
-      },
-      body: JSON.stringify({ path: location.pathname }),
-    }).catch((err) => console.error("Tracking error:", err));
+    fetch(`${BACKEND_URL}/api/track`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+  },
+  body: JSON.stringify({ path: location.pathname }),
+})
+.catch((err) => console.error("Tracking error:", err));
   }, [location]);
 }
